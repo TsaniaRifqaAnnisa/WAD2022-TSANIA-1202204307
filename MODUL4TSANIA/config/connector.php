6ac1c1da148565ab88af2b2192ce3de0b5b49221
TsaniaRifqaAnnisa <115614041+TsaniaRifqaAnnisa@users.noreply.github.com>
2022-11-27 23:04:38 +0700

    Delete MODUL4TSANIA directory

--- MODUL4TSANIA/config/connector.php
+++
@@ -1,9 +0,0 @@
-    <?php 
-        $dbhost = "localhost";
-        $dbuser = "root";
-        $dbname = "wad_modul4_tsania";
-        $dbpass = "";
-        $dbport = 3315;
-
-        $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport);
-    ?>
